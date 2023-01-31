ead00d2335b0e3aa26508296d9ee45093d8a170f
issaka-sanfo <96343967+issaka-sanfo@users.noreply.github.com>
2023-01-31 12:18:04 +0100

    Update pipelineJob.groovy

--- pipelineJob.groovy
+++ pipelineJob.groovy
@@ -3,7 +3,7 @@ pipeline {
     stages {
         stage('Build') {
             steps {
-                sh ''''
+                sh '''
                     docker compose version
                 '''
             }
